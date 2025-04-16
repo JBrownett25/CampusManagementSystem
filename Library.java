3c310dae0cbb651c00379b5504b6f9c3642f0287
Joel <jbrownett25@gmail.com>
2025-04-16 16:35:08 +0100

    Improved DetailsGUI and added the potential for a reserve feature.

--- Library.java
+++ Library.java
@@ -38,8 +38,17 @@ public class Library {
                 String line;
                while((line = reader.readLine()) != null) {
                    if (line.contains(find)) {
-                       System.out.println(line);
-                       detailsGUI(find, line);
+
+                       String name = line.split(",")[0];
+                       String author = line.split(",")[1];
+                       String course = line.split(",")[2];
+                       String copies = line.split(",")[3];
+
+                       detailsGUI(find, name, author, course, copies);
+                       break;
+
+
+
                    }
                }
             }catch (IOException e) {
@@ -49,19 +58,84 @@ public class Library {
 
         }
     }
-    public void detailsGUI(String find, String bookLine) {
+
+    JTextArea bookTitle, bookAuthor, bookCourse, bookCount;
+    JLabel bookTitleLabel, bookAuthorLabel, bookCourseLabel, bookCountLabel;
+
+    public void detailsGUI(String find, String name, String author, String course, String copies) {
+
         JFrame bookFrame; //Run a new frame
         bookFrame = new JFrame("Book Title: " + find);
         bookFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         bookFrame.setSize(600, 600);
 
         JPanel bookPanel = new JPanel();
+        bookPanel.setLayout(new BoxLayout(bookPanel, BoxLayout.Y_AXIS));
 
-        JTextArea BookDetails = new JTextArea();
-        BookDetails.setFont(new Font("Arial", Font.PLAIN, 30));
-        BookDetails.setEditable(false);
-        BookDetails.setText(bookLine); //Add details of the user's book to the TextArea.
-        bookPanel.add(BookDetails);
+        bookTitleLabel = new JLabel("Book Title: ");
+        bookTitleLabel.setFont(new Font("Arial", Font.PLAIN, 30));
+        bookPanel.add(bookTitleLabel);
+
+        bookTitle = new JTextArea();
+        bookTitle.setFont(new Font("Arial", Font.PLAIN, 30));
+        bookTitle.setEditable(false);
+        bookTitle.setText(name); //Add details of the user's book to the TextArea.
+        bookPanel.add(bookTitle);
+
+        bookAuthorLabel = new JLabel("Book Author: ");
+        bookAuthorLabel.setFont(new Font("Arial", Font.PLAIN, 30));
+        bookPanel.add(bookAuthorLabel);
+
+        bookAuthor = new JTextArea();
+        bookAuthor.setFont(new Font("Arial", Font.PLAIN, 30));
+        bookAuthor.setEditable(false);
+        bookAuthor.setText(author);
+        bookPanel.add(bookAuthor);
+
+        bookCourseLabel = new JLabel("Book Course: ");
+        bookCourseLabel.setFont(new Font("Arial", Font.PLAIN, 30));
+        bookPanel.add(bookCourseLabel);
+
+        bookCourse = new JTextArea();
+        bookCourse.setFont(new Font("Arial", Font.PLAIN, 30));
+        bookCourse.setEditable(false);
+        bookCourse.setText(course);
+        bookPanel.add(bookCourse);
+
+        bookCountLabel = new JLabel("Book Count: ");
+        bookCountLabel.setFont(new Font("Arial", Font.PLAIN, 30));
+        bookPanel.add(bookCountLabel);
+
+        bookCount = new JTextArea();
+        bookCount.setFont(new Font("Arial", Font.PLAIN, 30));
+        bookCount.setEditable(false);
+        bookCount.setText(copies);
+        bookPanel.add(bookCount);
+
+        JButton reserve = new JButton("reserve");
+        reserve.setFont(new Font("Arial", Font.PLAIN, 30));
+        reserve.addActionListener(e -> {
+
+            bookFrame.setVisible(false);
+
+            JFrame reserveFrame = new JFrame("Reserve Book");
+            reserveFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+            reserveFrame.setSize(600, 600);
+
+            JPanel reservePanel = new JPanel();
+
+            JTextArea reserveText = new JTextArea();
+            reserveText.setText( name + " Successfully reserved.\nPlease collect from the Library Help Desk.");
+            reserveText.setFont(new Font("Arial", Font.PLAIN, 30));
+            reserveText.setEditable(false);
+            reservePanel.add(reserveText);
+
+            reserveFrame.add(reservePanel);
+            reserveFrame.setVisible(true);
+
+
+        });
+        bookPanel.add(reserve);
 
         bookFrame.add(bookPanel);
         bookFrame.setVisible(true);
